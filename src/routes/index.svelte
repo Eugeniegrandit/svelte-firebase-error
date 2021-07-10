a304afa4df323672753dfa4b3d5955c8fe71093f
Eugeniegrandit <60859764+EcoleJean23@users.noreply.github.com>
2021-07-10 12:28:57 +0200

    onMount

--- src/routes/index.svelte
+++ src/routes/index.svelte
@@ -1,20 +1,21 @@
 <script>
     import { initializeApp } from "firebase/app"
-    const firebaseConfig = {
-        apiKey: "AIzaSyDOCAbC123dEf456GhI789jKl01-MnO",
-        authDomain: "myapp-project-123.firebaseapp.com",
-        databaseURL: "https://myapp-project-123.firebaseio.com",
-        projectId: "myapp-project-123",
-        storageBucket: "myapp-project-123.appspot.com",
-        messagingSenderId: "65211879809",
-        appId: "1:65211879909:web:3ae38ef1cdcb2e01fe5f0c",
-        measurementId: "G-8GSGZQ44ST"
-    }
-    const firebaseApp = initializeApp(firebaseConfig);
-</script>
+    import {onMount} from "svelte";
 
-<h1>Welcome to SvelteKit</h1>
-<p>Visit <a href="https://kit.svelte.dev">kit.svelte.dev</a> to read the documentation</p>
+    onMount(() => {
+        const firebaseConfig = {
+            apiKey: "AIzaSyDOCAbC123dEf456GhI789jKl01-MnO",
+            authDomain: "myapp-project-123.firebaseapp.com",
+            databaseURL: "https://myapp-project-123.firebaseio.com",
+            projectId: "myapp-project-123",
+            storageBucket: "myapp-project-123.appspot.com",
+            messagingSenderId: "65211879809",
+            appId: "1:65211879909:web:3ae38ef1cdcb2e01fe5f0c",
+            measurementId: "G-8GSGZQ44ST"
+        }
+        const firebaseApp = initializeApp(firebaseConfig);
+    })
+</script>
 
 <h1>Welcome to SvelteKit</h1>
 <p>Visit <a href="https://kit.svelte.dev">kit.svelte.dev</a> to read the documentation</p>
